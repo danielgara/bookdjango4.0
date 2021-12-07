f0de027c39a93f8ce637e26decded9489a511bc8
Daniel Correa <yo@danielgara.com>
2021-12-07 09:13:36 -0500

    imp: Chapter18

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from django.shortcuts import get_object_or_404
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')    
@@ -16,3 +17,7 @@ def about(request):
 def signup(request):
     email = request.GET.get('email')
     return render(request, 'signup.html', {'email':email})
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie,pk=movie_id)
+    return render(request, 'detail.html', {'movie':movie})
