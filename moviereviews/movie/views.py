84789b6d3ff356923ac830650b86402881ea5afb
Daniel Correa <yo@danielgara.com>
2021-12-07 09:35:48 -0500

    imp: complete

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Movie, Review
 from django.shortcuts import get_object_or_404, redirect
 from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')    
@@ -23,7 +24,8 @@ def detail(request, movie_id):
     movie = get_object_or_404(Movie,pk=movie_id)
     reviews = Review.objects.filter(movie = movie)
     return render(request, 'detail.html', {'movie':movie, 'reviews': reviews})
-    
+
+@login_required
 def createreview(request, movie_id):   
     movie = get_object_or_404(Movie,pk=movie_id) 
     if request.method == 'GET':
@@ -41,6 +43,7 @@ def createreview(request, movie_id):
             return render(request, 'createreview.html', 
               {'form':ReviewForm(),'error':'bad data passed in'})
 
+@login_required
 def updatereview(request, review_id):
     review = get_object_or_404(Review,pk=review_id,user=request.user) 
     if request.method =='GET':
@@ -56,6 +59,7 @@ def updatereview(request, review_id):
             return render(request, 'updatereview.html',
              {'review': review,'form':form,'error':'Bad data in form'})
 
+@login_required
 def deletereview(request, review_id):
     review = get_object_or_404(Review, pk=review_id, user=request.user)
     review.delete()
